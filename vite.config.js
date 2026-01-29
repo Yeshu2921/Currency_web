9768ea91852e2532e1f21a1af8a03d470b4cc9aa
Yeshambel <kdusbazezewmekonnen@gmail.com>
2026-01-29 00:15:50 -0800

    Change base path from '/Currency_system/' to '/Currency_web/'
    Updated base path for asset loading on GitHub Pages.

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Currency_system/', // Ensures assets load correctly on GitHub Pages
+  base: '/Currency_web/', // Ensures assets load correctly on GitHub Pages
   server: {
     port: 355,              // Fixed: Must be inside "server"
     strictPort: true,       // Fixed: Capital "P" and inside "server"
